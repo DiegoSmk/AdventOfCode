c412ac052c8bf3f2ac9926c053edb553c13be8c5
Diego Pena <54299885+DiegoSmk@users.noreply.github.com>
2024-12-05 05:55:49 -0300

    refactor

--- src/utils/readInput.kt
+++ src/utils/readInput.kt
@@ -1,7 +1,8 @@
 package utils
 
-import java.io.File
+import kotlin.io.path.Path
+import kotlin.io.path.readText
 
-fun readInput(name: String): List<String> {
-    return File("src/inputs/$name").readLines()
-}
+fun readInput(name: String): List<String> = Path("src/inputs/$name").readText().trim().lines()
+
+fun readInputAsString(name: String): String = Path("src/inputs/$name").readText().trim()
